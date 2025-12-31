7a0c80587a509ae79b2b4b0576e2ba8c9afd3f55
Manoj-kilari <manoj.kilari99@gmail.com>
2025-12-31 11:12:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1286,14 +1286,14 @@ const initialProducts = [
                       <rect width="20" height="16" x="2" y="4" rx="2"/>
                       <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/>
                     </svg>
-                    <span>Manoj.kilari77@gmail.com</span>
+                    <span>kilari.manoj010@gmail.com</span>
                   </div>
                   <div className="d-flex align-items-center gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                       <rect width="14" height="20" x="5" y="2" rx="2" ry="2"/>
                       <path d="M12 18h.01"/>
                     </svg>
-                    <span>+91 9502360627</span>
+                    <span>+91 6301601328</span>
                   </div>
                 </div>
               </div>
